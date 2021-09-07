5b8e96f9ca0020a5518eb12d96dbad077d485c20
jagonzalez25 <johans-123@hotmail.com>
2021-09-07 08:29:49 -0500

    Calculador - Eventos - Comunicacion entre componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,35 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  public title = 'lineaIII';
+  public title: string = 'Primer programa Angular';
+  numero1: number;
+  numero2: number;
+  resultado: number;
+
+  //var let const
+  public operar(op: string): void{
+     switch(op){
+           case 's':
+             this.resultado = this.numero1 + this.numero2;
+             break;
+           case 'r':
+              this.resultado = this.numero1 - this.numero2;   
+              break; 
+           case 'm':
+              this.resultado = this.numero1 * this.numero2;
+              break;
+           case 'd':
+              this.resultado = this.numero1 / this.numero2;    
+              break;   
+            default:
+              this.resultado = 0;                 
+      }
+  }
+
+  public teclear(e: any): void{
+     //console.log(e);
+     console.log("La tecla que se oprimio fue: " + e.key);
+     console.log("La palabra al momento es: " + e.target.value);
+  }
+
 }
